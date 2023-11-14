d80fb3afa82f1f1c4fd2a7c2618c0a2139297849
zaRT0 <andrey.panyavkin@mail.ru>
2023-11-14 21:09:48 +0400

    Добавьте файлы проекта.

---
+++ Second lab c++/Second lab c++.cpp
@@ -0,0 +1,8 @@
+﻿#include "Second lab c++.h"
+#include <ctime>
+using namespace std;
+using namespace list;
+int main()
+{
+	
+}
---
+++ Second lab c++/Second lab c++.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include <iostream>
+#include <stdexcept>
+#include <ctime>
+#include <cmath>
+using namespace std;
+namespace linkedList {
+
+    template<typename T>
+    class Node {
+    public:
+        T _cofficient;
+        Node* _next;
+        Node(T cofficient) :_cofficient(cofficient), _next(nullptr) {}
+    };
+
+    template <typename T>
+    class LinkedList {
+    private:
+        int _size;
+        Node<T>* _head;
+    public:
+        LinkedList() : _size(0), _head(nullptr) {}
+        LinkedList(int size, int upper_bound, int lover_bound);
+        LinkedList(int size);
+        ~LinkedList();
+    };
+}
